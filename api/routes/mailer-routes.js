b1dc64e6a392f81766763ca8b0167fc0b9e0bcc4
semperry <ryan.curtis88@gmail.com>
2019-05-17 22:05:49 -0600

    test the from email

--- api/routes/mailer-routes.js
+++ api/routes/mailer-routes.js
@@ -16,12 +16,12 @@ MailRouter.route('/').post((req, res) => {
   });
 
   let mailOptions = {
-    from: data.email,
+    from: '3017productions@gmail.com',
     to: '3017productions@gmail.com',
     subject: 'Message from Contact Form',
-    html: `<p>${data.name}</p>
-          <p>${data.email}</p>
-          <p>${data.message}</p>`
+    html: `<p>Message from: ${data.name}</p>
+          <p>Reply to: ${data.email}</p>
+          <p>Message: ${data.message}</p>`
   }
 
   smtpTransport.sendMail(mailOptions,
