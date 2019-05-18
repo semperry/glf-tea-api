d17fb036ed780e18e17f691bee401481bdafd176
semperry <ryan.curtis88@gmail.com>
2019-05-17 20:50:15 -0600

    Route added for nodemailer for contact form

---
+++ api/routes/mailer-routes.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const MailRouter = express.Router();
+const nodemailer = require('nodemailer')
+
+MailRouter.route('/contact').post((req, res) => {
+  let data = req.data
+
+  let smtpTransport = nodemailer.createTransport({
+    service: 'Gmail',
+    port: 465,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS
+    }
+  });
+
+  let mailOptions = {
+    from: data.email,
+    to: '3017productions@gmail.com',
+    subject: 'Message from Contact Form',
+    html: `<p>${data.name}</p>
+          <p>${data.email}</p>
+          <p>${data.message}</p>`
+  }
+
+  smtpTransport.sendMail(mailOptions,
+    (error, response)=> {
+      if(error) {
+        res.send(error)
+      } else {
+        res.send('Success')
+      }
+      smtpTransport.close()
+    })
+})
+
+module.exports(MailRouter)
--- api/server.js
+++ api/server.js
@@ -8,6 +8,7 @@ const mongoose = require("mongoose");
 const teaRoutes = require("./routes/tea-routes");
 const cartRoutes = require("./routes/cart-routes")
 const subRoutes = require("./routes/subscription-routes")
+const mailRoutes = require("./routes/mailer-routes")
 
 mongoose.Promise = global.Promise;
 mongoose
@@ -26,6 +27,7 @@ app.use(bodyParser.json());
 app.use("/tea", teaRoutes);
 app.use("/cart", cartRoutes)
 app.use("/subscribers", subRoutes)
+app.use("/contact", mailRoutes)
 
 app.listen(PORT, function() {
   console.log("Server is running on Port:", PORT);
